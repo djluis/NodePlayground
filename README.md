5695159afd3c07eadcabeaf26ff9b0265b0cad00
Daniel Luis (Shanks) <dani.jluis@gmail.com>
2015-06-24 00:27:49 +0100

    Set Mongo Db directory and test data.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # NodePlayground
+
+# Requirements
+
+- MongoDB 3.0.4+
+- Node v0.12.5+
+
+# Notes 
+
+- Generate Random Data 
+http://www.json-generator.com/
+Template:
+[
+  '{{repeat(5, 100)}}',
+  {
+    name: '{{firstName()}} {{surname()}}',
+    company: '{{company().toUpperCase()}}',
+    email: '{{email()}}',
+    phone: '+1 {{phone()}}',
+    address: '{{integer(100, 999)}} {{street()}}, {{city()}}, {{state()}}, {{integer(100, 10000)}}'
+  }
+]
+
+
+
---
+++ data/data.js
@@ -0,0 +1,177 @@
+[
+  {
+    "name": "Erickson Richardson",
+    "company": "QUORDATE",
+    "email": "ericksonrichardson@quordate.com",
+    "phone": "+1 (836) 500-3007",
+    "address": "225 Vine Street, Turah, Massachusetts, 6088"
+  },
+  {
+    "name": "Lacy Golden",
+    "company": "EMOLTRA",
+    "email": "lacygolden@emoltra.com",
+    "phone": "+1 (887) 442-3382",
+    "address": "699 Jackson Court, Hampstead, Idaho, 6193"
+  },
+  {
+    "name": "Lynnette Mckay",
+    "company": "CHILLIUM",
+    "email": "lynnettemckay@chillium.com",
+    "phone": "+1 (802) 457-2820",
+    "address": "881 Withers Street, Brooktrails, South Carolina, 3653"
+  },
+  {
+    "name": "Janelle Mccullough",
+    "company": "ESCENTA",
+    "email": "janellemccullough@escenta.com",
+    "phone": "+1 (831) 535-2968",
+    "address": "821 Delevan Street, Brecon, Alabama, 5901"
+  },
+  {
+    "name": "Martina Oneill",
+    "company": "ANARCO",
+    "email": "martinaoneill@anarco.com",
+    "phone": "+1 (945) 532-2182",
+    "address": "447 Albany Avenue, Harrison, Guam, 5783"
+  },
+  {
+    "name": "Lindsay Young",
+    "company": "BOLAX",
+    "email": "lindsayyoung@bolax.com",
+    "phone": "+1 (959) 431-2179",
+    "address": "270 Clifford Place, Calpine, Ohio, 762"
+  },
+  {
+    "name": "Carroll Obrien",
+    "company": "BEDLAM",
+    "email": "carrollobrien@bedlam.com",
+    "phone": "+1 (959) 458-3398",
+    "address": "216 Lincoln Avenue, Richmond, Louisiana, 5989"
+  },
+  {
+    "name": "Walters Snyder",
+    "company": "PURIA",
+    "email": "walterssnyder@puria.com",
+    "phone": "+1 (817) 491-2524",
+    "address": "452 Coleman Street, Odessa, Iowa, 5439"
+  },
+  {
+    "name": "Sonya Madden",
+    "company": "DEMINIMUM",
+    "email": "sonyamadden@deminimum.com",
+    "phone": "+1 (917) 484-2370",
+    "address": "141 Furman Avenue, Foscoe, Wisconsin, 2488"
+  },
+  {
+    "name": "Charmaine Yates",
+    "company": "ROTODYNE",
+    "email": "charmaineyates@rotodyne.com",
+    "phone": "+1 (871) 430-2844",
+    "address": "120 Kosciusko Street, Yardville, Marshall Islands, 6624"
+  },
+  {
+    "name": "Campbell Burt",
+    "company": "BOVIS",
+    "email": "campbellburt@bovis.com",
+    "phone": "+1 (932) 550-3578",
+    "address": "741 Vandervoort Place, Yettem, Florida, 4176"
+  },
+  {
+    "name": "Gabriela Manning",
+    "company": "PROTODYNE",
+    "email": "gabrielamanning@protodyne.com",
+    "phone": "+1 (972) 412-3399",
+    "address": "316 Fleet Walk, Johnsonburg, California, 875"
+  },
+  {
+    "name": "Barker Short",
+    "company": "VELOS",
+    "email": "barkershort@velos.com",
+    "phone": "+1 (936) 446-3224",
+    "address": "814 Empire Boulevard, Vienna, Arkansas, 9285"
+  },
+  {
+    "name": "Eaton Baxter",
+    "company": "FUTURIZE",
+    "email": "eatonbaxter@futurize.com",
+    "phone": "+1 (999) 498-2114",
+    "address": "551 Cozine Avenue, Volta, Washington, 6395"
+  },
+  {
+    "name": "Roberson Knox",
+    "company": "SECURIA",
+    "email": "robersonknox@securia.com",
+    "phone": "+1 (952) 512-3482",
+    "address": "641 Lois Avenue, Tampico, Hawaii, 2916"
+  },
+  {
+    "name": "Diann Morgan",
+    "company": "NAXDIS",
+    "email": "diannmorgan@naxdis.com",
+    "phone": "+1 (946) 552-2250",
+    "address": "735 Christopher Avenue, Groveville, Oklahoma, 649"
+  },
+  {
+    "name": "Key Cleveland",
+    "company": "NETUR",
+    "email": "keycleveland@netur.com",
+    "phone": "+1 (838) 514-2962",
+    "address": "243 Cypress Court, Juntura, Pennsylvania, 5449"
+  },
+  {
+    "name": "Tamika Bailey",
+    "company": "FLUM",
+    "email": "tamikabailey@flum.com",
+    "phone": "+1 (819) 514-3208",
+    "address": "104 Miami Court, Sussex, Rhode Island, 7167"
+  },
+  {
+    "name": "Tracy Tran",
+    "company": "INFOTRIPS",
+    "email": "tracytran@infotrips.com",
+    "phone": "+1 (828) 569-3653",
+    "address": "211 Hyman Court, Blanford, New Hampshire, 1064"
+  },
+  {
+    "name": "Dawson Turner",
+    "company": "VIASIA",
+    "email": "dawsonturner@viasia.com",
+    "phone": "+1 (814) 444-2537",
+    "address": "515 Atlantic Avenue, Belva, Nevada, 9225"
+  },
+  {
+    "name": "Celia Douglas",
+    "company": "MANGLO",
+    "email": "celiadouglas@manglo.com",
+    "phone": "+1 (987) 516-2716",
+    "address": "975 Harway Avenue, Thatcher, Puerto Rico, 6338"
+  },
+  {
+    "name": "French Workman",
+    "company": "ZAGGLES",
+    "email": "frenchworkman@zaggles.com",
+    "phone": "+1 (903) 581-3530",
+    "address": "362 Jamison Lane, Dalton, Wyoming, 5272"
+  },
+  {
+    "name": "Joyce Long",
+    "company": "PHEAST",
+    "email": "joycelong@pheast.com",
+    "phone": "+1 (845) 460-2513",
+    "address": "320 Senator Street, Mappsville, North Carolina, 8805"
+  },
+  {
+    "name": "Oneal Travis",
+    "company": "FLOTONIC",
+    "email": "onealtravis@flotonic.com",
+    "phone": "+1 (895) 481-3034",
+    "address": "823 Schenck Avenue, Oceola, Northern Mariana Islands, 4426"
+  },
+  {
+    "name": "Lynch Mccoy",
+    "company": "MUSIX",
+    "email": "lynchmccoy@musix.com",
+    "phone": "+1 (836) 430-2206",
+    "address": "236 Johnson Street, Bison, Mississippi, 4719"
+  }
+]
